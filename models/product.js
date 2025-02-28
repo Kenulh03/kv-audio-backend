560ccd86895d0c422569488a2094a32f596b32a2
Kenulh03 <kenulfernando123@gmail.com>
2025-02-28 14:43:21 +0530

    updated product.js

--- models/product.js
+++ models/product.js
@@ -33,7 +33,7 @@ const productSchema = new mongoose.Schema({
     },
     image:{
         type:[String],
-        requred:true,
+        required:true,
         default:["https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2247726673.jpg"]
     }
 })
